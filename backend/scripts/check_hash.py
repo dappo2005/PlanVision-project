d63c99cd7d7f65e63cbaa4f9dbd39ddc675bf13c
dappo2005 <daf.maula123@gmail.com>
2025-11-23 15:30:36 +0700

    coba

--- backend/scripts/check_hash.py
+++
@@ -1,8 +0,0 @@
-import os, bcrypt
-h = os.environ.get('HASH','').encode()
-p = os.environ.get('PASS','').encode()
-print('hash_len', len(h))
-try:
-    print('check', bcrypt.checkpw(p, h))
-except Exception as e:
-    print('error', type(e).__name__, str(e))
